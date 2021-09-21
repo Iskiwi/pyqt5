c595f663db2c59c9df1ed26bb5e91ad9301bc308
Xiao Yi <2102004995@qq.com>
2021-09-21 11:20:38 +0800

    为窗口设置图标

---
+++ add_icon.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/20 16:34
+# @Author  : B612
+# @File    : add_icon.py
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('I:\pyqt5_demo\pyQt5\images\ta....ico'))
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
