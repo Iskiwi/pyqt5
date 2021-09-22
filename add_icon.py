b9a2158feb04cceea1517335863e9fde8f82307f
Xiao Yi <2102004995@qq.com>
2021-09-22 17:19:26 +0800

    图标的绝对路径前加上 r 转义

--- add_icon.py
+++ add_icon.py
@@ -18,7 +18,7 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('I:\pyqt5_demo\pyQt5\images\ta....ico'))
+        self.setWindowIcon(QIcon(r'I:\pyqt5_demo\pyQt5\images\ta....ico'))
 
         self.show()
 
