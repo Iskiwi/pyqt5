f09331ed59bf6634c67b73ec285ee82da6a17f8b
Xiao Yi <2102004995@qq.com>
2021-10-03 21:11:44 +0800

    错误的退出程序

--- pyQt5/组件/slider的RGB调色板.py
+++ pyQt5/组件/slider的RGB调色板.py
@@ -20,10 +20,30 @@ from PyQt5.QtGui import QColor
 """
 
 
+import sys
+from PyQt5.QtWidgets import (QWidget, QSlider, QPushButton, QHBoxLayout, QGridLayout,
+                             QLabel, QApplication, QFrame)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+
+"""
+    这个例子是通过slider来创建一个RGB的调色板
+    思路：
+    1、slider的value是0-99 /int型， RGB的取值是0-255
+    value/100*256 可能结果会是float型，所以取整
+    2、可以直接 QColor(a, b, c)
+    但是可以复习切换按键 
+    3、包括 slider button 标签显示各元素的大小 frame 
+"""
+
+
 class Example(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.r = 0
+        self.g = 0
+        self.b = 0
         self.initUI()
 
     def initUI(self):
@@ -65,7 +85,7 @@ class Example(QWidget):
 
         self.frame = QFrame(self)
         self.col = QColor(0, 0, 0)
-        self.frame.setGeometry(300, 50, 150, 150)
+        self.frame.setGeometry(350, 50, 150, 150)
         self.frame.setStyleSheet("QFrame { background-color: %s }" %
                                   self.col.name())
 
@@ -74,28 +94,26 @@ class Example(QWidget):
         self.show()
 
     def setR(self, value):
-        val = int(value * 256 / 100)
-        self.Rlbl.setText(val)
+        self.r = int(value * 256 / 100)
 
     def setG(self, value):
-        val = int(value * 256 / 100)
-        self.Glbl.settext(val)
+        self.g = int(value * 256 / 100)
 
     def setB(self, value):
-        val = int(value * 256 / 100)
-        self.Blbl.setText(val)
+        self.b = int(value * 256 / 100)
+
 
     def showvalue(self, pressed):
         object = self.sender()
         if object.text() == "R":
             if pressed:
-                self.col.setRed(self.Rlbl.getText())
+                self.col.setRed(self.r)
         elif object.text() == "G":
             if pressed:
-                self.col.setGreen(self.Glbl.getText())
+                self.col.setGreen(self.g)
         else:
             if pressed:
-                self.col.setBlue(self.Blbl.getText())
+                self.col.setBlue(self.b)
         self.frame.setStyleSheet("QFrame { background-color: %s }" %
                                      self.col.name())
 
