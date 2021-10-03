aa0204fbbdce68d7255c8cea77583e02ad875788
Xiao Yi <2102004995@qq.com>
2021-10-03 21:42:05 +0800

    删除了一些功能，可以正常使用了。

--- pyQt5/组件/slider的RGB调色板.py
+++ pyQt5/组件/slider的RGB调色板.py
@@ -20,23 +20,6 @@ from PyQt5.QtGui import QColor
 """
 
 
-import sys
-from PyQt5.QtWidgets import (QWidget, QSlider, QPushButton, QHBoxLayout, QGridLayout,
-                             QLabel, QApplication, QFrame)
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor
-
-"""
-    这个例子是通过slider来创建一个RGB的调色板
-    思路：
-    1、slider的value是0-99 /int型， RGB的取值是0-255
-    value/100*256 可能结果会是float型，所以取整
-    2、可以直接 QColor(a, b, c)
-    但是可以复习切换按键 
-    3、包括 slider button 标签显示各元素的大小 frame 
-"""
-
-
 class Example(QWidget):
 
     def __init__(self):
